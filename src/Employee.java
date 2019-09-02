7e6e7965e13fd93413c7eb83747aeb314ca6c48f
RustyRaptor <ziad.arafat@etvamerica.com>
2019-09-02 15:33:23 -0600

    changes

--- src/Employee.java
+++ src/Employee.java
@@ -9,6 +9,7 @@ public class Employee {
 	private String state;
 	private int zipCode;
 	private int advisors[];
+
 	public Employee() {
 		this.name = null;
 		this.state = null;
@@ -17,49 +18,60 @@ public class Employee {
 		this.age = 0;
 		this.advisors = null;
 	}
+
 	public Employee(Object obj) {
 		if (obj == null) {
 			return;
 		}
 		if (obj instanceof Employee) {
-			Employee emp = (Employee)obj;
+			Employee emp = (Employee) obj;
 			name = emp.name;
 			state = emp.state;
 			zipCode = emp.zipCode;
 			no = emp.no;
 			age = emp.age;
 			advisors = emp.advisors;
-			
-			
+
 		}
 	}
+
+	// getters and setters
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getNo() {
 		return no;
 	}
+
 	public void setNo(int no) {
 		this.no = no;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public int getZipCode() {
 		return zipCode;
 	}
+
 	public void setZipCode(int zipCode) {
 		this.zipCode = zipCode;
 	}
@@ -67,21 +79,18 @@ public class Employee {
 	public int[] getAdvisors() {
 		return advisors;
 	}
+
 	public void setAdvisors(int[] advisors) {
 		this.advisors = advisors;
 	}
+
+	// returns a string describing all the values.
 	@Override
 	public String toString() {
 		return "Employee [name=" + name + ", no=" + no + ", age=" + age + ", state=" + state + ", zipCode=" + zipCode
 				+ ", advisors=" + Arrays.toString(advisors) + "]";
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + no;
-		return result;
-	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if ((obj == null)) {
@@ -101,6 +110,7 @@ public class Employee {
 		}
 		return true;
 	}
+
 	public static void getAllAdvisors(Employee e1, Employee e2) {
 		if ((e1 == null) || (e2 == null)) {
 //			return null;
@@ -112,28 +122,28 @@ public class Employee {
 //			return e1.getAdvisors();
 			System.out.println(e1.getAdvisors());
 		}
-		if (!(e1 instanceof Employee) || !(e2 instanceof Employee) ) {
+		if (!(e1 instanceof Employee) || !(e2 instanceof Employee)) {
 //			return null;
 			System.out.println("NULL");
-		}		
+		}
 		List<int[]> merged = new ArrayList<int[]>(Arrays.asList(e1.getAdvisors()));
 		merged.addAll(Arrays.asList(e2.getAdvisors()));
 		System.out.println(merged);
-		
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		Employee Ziad = new Employee();
-		Employee Brianna = new Employee();
+		Employee Tim = new Employee();
 		System.out.println(Ziad.getAdvisors());
-		int ziadAdvNew[] = {0,0,0};
+		int ziadAdvNew[] = { 0, 0, 0 };
 		Ziad.setAdvisors(ziadAdvNew);
 		System.out.println(ziadAdvNew);
 		System.out.println(Arrays.toString(Ziad.getAdvisors()));
-		Brianna.setAdvisors(ziadAdvNew);
-		
-		Employee.getAllAdvisors(Ziad, Brianna);
+//		Tim.setAdvisors(ziadAdvNew);
+		Tim.no = 999;
+		Employee.getAllAdvisors(Ziad, Tim);
+		System.out.println(Ziad.equals(Tim));
 	}
-	
+
 }
