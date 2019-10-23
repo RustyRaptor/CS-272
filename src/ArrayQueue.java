984e8f3c140f72ba2470a22a068998d80998f1cf
RustyRaptor <ziada@nmsu.edu>
2019-10-23 12:36:47 -0600

    Started Lab 9

---
+++ src/ArrayQueue.java
@@ -0,0 +1,27 @@
+public class ArrayQueue <E> implements QueueInterface {
+
+	@Override
+	public void enqueue(Object o) {
+
+	}
+
+	@Override
+	public Object dequeue() {
+		return null;
+	}
+
+	@Override
+	public Object front() {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+}
